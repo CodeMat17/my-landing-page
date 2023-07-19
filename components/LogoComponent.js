6a0ea23f1e1ba031e7955e16a2c425fb9c6449eb
CodeMat <codemat.biz@gmail.com>
2023-07-19 17:07:41 +0100

    updated logo component

--- components/LogoComponent.js
+++ components/LogoComponent.js
@@ -18,9 +18,16 @@ const LogoComponent = () => {
       <div className='relative flex items-center justify-center w-[95px] h-[95px] animate-spin-slow'>
         <Image alt='logo' fill src='/web-dev-logo.webp' />
       </div>
-      <div className=' flex items-center justify-center text-3xl font-bold absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full bg-[#031e39] dark:bg-[#F2AA4C] text-[#F2AA4C] dark:text-[#031e39] px-2 py-1'>
-        <p className={cookie.className}>CM</p>
+
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className='relative w-20 h-14 mb-2'>
+          <Image alt='logo' fill priority src='/logo.webp' />
+        </div>
       </div>
+
+      {/* <div className=' flex items-center justify-center text-3xl font-bold absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full bg-[#031e39] dark:bg-[#F2AA4C] text-[#F2AA4C] dark:text-[#031e39] px-2 py-1'> */}
+        {/* <p className={cookie.className}>CM</p> */}
+      {/* </div> */}
     </div>
   );
 };
